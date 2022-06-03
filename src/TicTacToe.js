babd6ddc62dfde8c3b163468e18e76ff0a2732b8
Belgef <girnyimykola@gmail.com>
2022-06-03 21:03:57 +0300

    Added sockets

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,12 +1,10 @@
-import { React, useState } from 'react'
-import {
-    useNavigate,
-    useLocation,
-    useParams
-} from "react-router-dom";
+import { React, useState, useEffect } from 'react'
+import { useNavigate, useLocation, useParams, useSearchParams } from "react-router-dom";
 import { v1 as uuidv1 } from 'uuid';
+import { socket } from './context/socket';
 
 export function TicTacToeLobby() {
+    let room = useParams()
     const [username, setUsername] = useState('');
     let location = useLocation();
     let navigate = useNavigate();
@@ -17,31 +15,53 @@ export function TicTacToeLobby() {
                 <input type='text' placeholder='Enter your name...' onChange={(e) => setUsername(e.target.value)}></input>
             </form>
             <div>
-                <button onClick={() => navigate(`${location.pathname}/create?username=${username}`)}>Create room</button>
+                {room.id === undefined ? (
+                    <button onClick={`${location.pathname + (location.pathname.endsWith('/') ? '' : '/')}create?username=${username}`}>Create room</button>
+                ) : (
+                    <button onClick={() => navigate(`/tic-tac-toe/room/${room.id}?username=${username}`)}>Join room</button>
+                )}
             </div>
         </div>
     );
 }
 
-export function TicTacToeCreateRoom() {
+export function TicTacToeHostRoom() {
     let room = uuidv1();
-    let joinUrl = window.location.host+'/tic-tac-toe/join/'+room;
+    let joinUrl = window.location.host + '/tic-tac-toe/' + room;
+    const [searchParams, setSearchParams] = useSearchParams();
+    let username = searchParams.get("username")
+    useEffect(() => {
+        let s = socket()
+        s.emit('join', room, username);
+        s.on('gethost', id => s.emit('host', id, username));
+        s.on('message', message => console.log(message));
+        return () => s.disconnect();
+    })
 
     return (
         <div>
             <h1>Create new room</h1>
             <p>Give your friend a room id: {room}</p>
-            <p>Or let them join via url: <input type='url' readOnly='true' value={joinUrl} /><button onClick={()=>navigator.clipboard.writeText(joinUrl)}>Copy</button></p>
+            <p>Or let them join via url: <input type='url' readOnly='true' value={joinUrl} /><button onClick={() => navigator.clipboard.writeText(joinUrl)}>Copy</button></p>
         </div>
     );
 }
 
 export function TicTacToeJoinRoom() {
-    let room = useParams()
-
+    let params = useParams();
+    let [host, setHost] = useState('');
+    const [searchParams, setSearchParams] = useSearchParams();
+    let username = searchParams.get("username")
+    useEffect(() => {
+        let s = socket()
+        s.emit('join', params.id, username);
+        s.on('host', message => setHost(message));
+        s.on('message', message => console.log(message));
+        return () => s.disconnect();
+    })
     return (
         <div>
-            <h1>Room of ... ({room.id})</h1>
+            <h1>Room of {host}</h1>
             <p>Waiting for host to start game</p>
         </div>
     );
