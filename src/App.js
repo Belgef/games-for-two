429bd76502b95f9638cc9fbbb084618baaf1187c
Kostyshyn-Max <mk0996417892@gmail.com>
2022-06-03 21:00:48 +0300

    Add some styles

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import { Link, Outlet } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Games for two</Link>
-          </li>
-          <li>
-            <Link to="/tic-tac-toe">Tic Tac Toe</Link>
-          </li>
-          <li>
-            <Link to="/bulls-and-cows">Bulls & Cows</Link>
-          </li>
-          <li>
-            <Link to="/hangman">Hangman</Link>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{backgroundColor: "#e3f2fd"}}>
+        <div className="container-fluid">
+          <Link className="navbar-brand font-exo" to="/">Games for two</Link>
+          <button className="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item active">
+                <Link className="nav-link font-exo" to="/tic-tac-toe">Tic Tac Toe</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link font-exo" to="/bulls-and-cows">Bulls & Cows</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link font-exo" to="/hangman">Hangman</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
       <Outlet />
     </div>
--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -11,13 +11,20 @@ export function TicTacToeLobby() {
     let location = useLocation();
     let navigate = useNavigate();
     return (
-        <div>
-            <h1>Tic Tac Toe</h1>
-            <form>
-                <input type='text' placeholder='Enter your name...' onChange={(e) => setUsername(e.target.value)}></input>
-            </form>
-            <div>
-                <button onClick={() => navigate(`${location.pathname}/create?username=${username}`)}>Create room</button>
+        <div className="card center" style={{ width: "28rem" }}>
+            <div className="card-body">
+                <center>
+                    <h3 className="card-title font-baumans" style={{ fontWeight: "bold" }}>Tic Tac Toe</h3>
+                </center>
+
+                <div className="form-group ">
+                    <input type="text" className="form-control narrow" id="username" placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)} />
+                </div>
+
+                <div className="tic-tac-toe-game-buttons">
+                    <button className="btn btn-primary font-baumans btn-sm" style={{ margin: "0 1rem" }} onClick={() => navigate(`${location.pathname + (location.pathname.endsWith('/') ? '' : '/')}create?username=${username}`)}>Create Game</button>
+                    <a href="#" className="btn btn-primary font-baumans btn-sm" style={{ margin: "0 1rem" }}>Join Game</a>
+                </div>
             </div>
         </div>
     );
@@ -25,16 +32,84 @@ export function TicTacToeLobby() {
 
 export function TicTacToeCreateRoom() {
     let room = uuidv1();
-    let joinUrl = window.location.host+'/tic-tac-toe/join/'+room;
+    let joinUrl = window.location.host + '/tic-tac-toe/join/' + room;
 
     return (
         <div>
-            <h1>Create new room</h1>
-            <p>Give your friend a room id: {room}</p>
-            <p>Or let them join via url: <input type='url' readOnly='true' value={joinUrl} /><button onClick={()=>navigator.clipboard.writeText(joinUrl)}>Copy</button></p>
+            <div className="card center" style={{ width: "28rem" }}>
+                <div className="card-body">
+                    <center>
+                        <h3 className="card-title font-baumans" style={{ fontWeight: "bold" }}>Tic Tac Toe Room</h3>
+                    </center>
+
+                    <div className="input-group input-group-sm mb-3">
+                        <span className="input-group-text font-exo narrow" id="basic-addon2" style={{ backgroundColor: "white", border: "none" }}>Game id: </span>
+                        <input type="text" className="form-control font-exo narrow" placeholder="Game ID" aria-label="Recipient's username" value={room} aria-describedby="basic-addon2" />
+                        <CopyButton copyValue={room} />
+                    </div>
+
+                    <div className="input-group input-group-sm mb-3">
+                        <span className="input-group-text font-exo narrow" id="popoverbtns" style={{ backgroundColor: "white", border: "none" }}>Or copy this link: </span>
+                        <input type='text' value={joinUrl} className="form-control font-exo narrow" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                        <CopyButton copyValue={joinUrl} />
+                    </div>
+
+                    <center>
+                        <h5 className="card-title font-baumans" style={{ fontWeight: "bold" }}>Players: </h5>
+                    </center>
+
+                    <center>
+                        <table class="table" style={{ width: "20rem" }}>
+                            <thead>
+                                <tr>
+                                    <th scope="col" style={{ width: "1rem" }}>#</th>
+                                    <th scope="col">Player nickname</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </center>
+
+                    <div className="tic-tac-toe-game-buttons">
+                        <button className="btn btn-primary btn-sm font-baumans" style={{ margin: "0 1rem" }}>Start Game</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
+
+function CopyButton(props) {
+    var icons = [
+        (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+        </svg>),
+        (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+        </svg>)
+    ];
+    let [i, setI] = useState(0);
+    return (
+        <button className="input-group-text font-exo narrow" type="button" onClick={() => {
+            navigator.clipboard.writeText(props.copyValue);
+            setI(1);
+            setTimeout(() => setI(0), 3000);
+        }}>
+            {icons[i]}
+        </button>)
+}
+
+
 
 export function TicTacToeJoinRoom() {
     let room = useParams()
